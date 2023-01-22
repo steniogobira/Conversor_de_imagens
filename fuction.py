7f321f68a6c57b8104c93394d9f6dca91bb1a861
steniogobira <steniogobira@gmail.com>
2023-01-22 20:30:36 -0300

    Reestruturação total - novas features + interface

--- fuction.py
+++ fuction.py
@@ -1,57 +1,164 @@
 import os
 from PIL import Image
 
-path = '/Users/gobis/Desktop/TESTE'
-erro = []
 
+errojpge = []
+erropng = []
+errogif = []
+
+convertidasjpge = []
+convertidaspng = []
+convertidasgif = []
+
+
+def create_log(log_text):
+    with open("conversion_log.txt", "a") as log_file:
+        log_file.write('\n' + log_text)
+
+
+def del_filetext():
+    with open("conversion_log.txt", "w") as log_file:
+        log_file.write('')
 
 
 def to_jpeg(path):
-    global erro
+    create_log('='*20)
+    create_log('\nCONVERSÂO PARA JPGE:\n')
+    create_log('='*20)
+    global errojpge, list_img, i
     list_files = os.listdir(path)
     list_img = [i for i in list_files if i.split(
         '.')[-1] != 'jpeg' or 'JPEG']
 
     i = 1
     for img in list_img:
-        print(
-            f'Convertendo imagem {img}. \n{i}/{len(list_img)} imagens'
-        )
+
         try:
-            input = path + img
-            output = path + img.replace(img.split('.')[-1], '.jpeg')
-            im = Image.open(input).convert("RGB")
-            im.save(output)
+            input = path + '/' + img
+            output = path + '/jpge/' + img.replace(img.split('.')[-1], '.jpeg')
+            if os.path.exists(path + '/jpge'):
+                im = Image.open(input).convert("RGB")
+                im.save(output)
+                convertidasjpge.append(img)
+            else:
+                os.mkdir(path + '/jpge')
+                im = Image.open(input).convert("RGB")
+                im.save(output)
+                convertidasjpge.append(img)
             i += 1
 
         except:
-            erro.append(img.split(".")[-1])
-            print(
-                f'Desculpa, não posso converter o arquivo om extensão {img.split(".")[-1]}'
-            )
-
-        else:
+            errojpge.append(img)
+            create_log(f'\n Desculpa, não posso converter o arquivo {img}\n ')
             continue
+    if len(errojpge) == 0:
+        create_log(
+            "\n Todas as imagens foram convertidas com sucesso para Jpeg ;)\n ")
+
+    else:
+        create_log('\nARQUIVOS CONVERTIDOS PARA JPEG:\n')
+        for n in range(0, (len(convertidasjpge))):
+            if len(convertidasjpge) != 0:
+                img_convertidas = str(convertidasjpge[n])
+                create_log((img_convertidas))
+        create_log('\nARQUIVOS NÃO CONVERTIDoS PARA JPEG: \n')
+        for j in range(0, len(errojpge)):
+            img_erro = str(errojpge[j])
+            create_log((img_erro))
 
 
-def arquivos_in(path):
+def to_png(path):
+    create_log('='*20)
+    create_log('\nCONVERSÂO PARA PNG:\n')
+    create_log('='*20)
+    global erropng, list_img, i
+    list_files = os.listdir(path)
+    list_img = [i for i in list_files if i.split(
+        '.')[-1] != 'png' or 'PNG']
 
-    global erro
-    os.chdir(path)
-    lista_arquivos = os.listdir()
+    i = 1
+    for img in list_img:
 
-    for arquivos in lista_arquivos:
-        if os.path.isfile(arquivos):
-            extensoes = arquivos.split('.')[-1]
-            if extensoes not in erro:
-                if os.path.exists(os.getcwd() + '/' + extensoes):
-                    os.rename(arquivos, os.getcwd() + '/' +
-                              extensoes + '/' + arquivos)
-                else:
-                    os.mkdir(arquivos.split('.')[-1])
-                    os.rename(arquivos, os.getcwd() + '/' +
-                              extensoes + '/' + arquivos)
+        try:
+            input = path + '/' + img
+            output = path + '/png/' + img.replace(img.split('.')[-1], '.png')
+            if os.path.exists(path + '/png'):
+                im = Image.open(input).convert("RGB")
+                im.save(output)
+                convertidaspng.append(img)
+
+            else:
+                os.mkdir(path + '/png')
+                im = Image.open(input).convert("RGB")
+                im.save(output)
+                convertidaspng.append(img)
+
+            i += 1
+
+        except:
+            erropng.append(img)
+            create_log(f'\n Desculpa, não posso converter o arquivo {img}\n ')
+            continue
+    if len(erropng) == 0:
+        create_log(
+            "\n Todas as imagens foram convertidas com sucesso para png ;)\n ")
+
+    else:
+        create_log('\nARQUIVOS CONVERTIDOS PARA PNG:\n')
+        for n in range(0, (len(convertidaspng))):
+            if len(convertidaspng) != 0:
+                img_convertidas = str(convertidaspng[n])
+                create_log((img_convertidas))
+        create_log('\nARQUIVOS NÃO CONVERTIDAS PARA PNG: \n')
+        for j in range(0, len(erropng)):
+            img_erro = str(erropng[j])
+            create_log((img_erro))
 
 
-to_jpeg(path)
-arquivos_in(path)
+def to_gif(path):
+
+    create_log('='*20)
+    create_log('\nCONVERSÂO PARA GIF:\n')
+    create_log('='*20)
+    global errogif, list_img, i
+    list_files = os.listdir(path)
+    list_img = [i for i in list_files if i.split(
+        '.')[-1] != 'gif' or 'GIF']
+
+    i = 1
+    for img in list_img:
+
+        try:
+            input = path + '/' + img
+            output = path + '/gif/' + img.replace(img.split('.')[-1], '.gif')
+            if os.path.exists(path + '/gif'):
+                im = Image.open(input).convert("RGB")
+                im.save(output)
+                convertidasgif.append(img)
+
+            else:
+                os.mkdir(path + '/gif')
+                im = Image.open(input).convert("RGB")
+                im.save(output)
+                convertidasgif.append(img)
+
+            i += 1
+
+        except:
+            errogif.append(img)
+            create_log(f'\n Desculpa, não posso converter o arquivo {img}\n ')
+            continue
+    if len(errogif) == 0:
+        create_log(
+            "\n Todas as imagens foram convertidas com sucesso para gif ;)\n ")
+
+    else:
+        create_log('\nARQUIVOS CONVERTIDOS PARA GIF:\n')
+        for n in range(0, (len(convertidasgif))):
+            if len(convertidasgif) != 0:
+                img_convertidas = str(convertidasgif[n])
+                create_log((img_convertidas))
+        create_log('\nARQUIVOS NÂO CONVERTIDOS PARA GIF: \n')
+        for j in range(0, len(errogif)):
+            img_erro = str(errogif[j])
+            create_log((img_erro))
--- main.py
+++ main.py
@@ -1,60 +1,137 @@
 import tkinter as ttk
 from tkinter import filedialog
+import fuction
+from tkinter.ttk import Progressbar
 import os
+import time
 
-pasta = ''
+path = os.getcwd()
 
 
 def get_diretorio():
-    global pasta
-    pasta = str(filedialog.askdirectory(title='selecione'))
-    Label02['text'] = pasta
-    Label03['text'] = "Diretório selecionado!"
+
+    global path, barra_progresso, atualizar_progresso
+
+    fuction.del_filetext()
+    barra_progresso.config(value=0)
+    atualizar_progresso()
+
+    path = str(filedialog.askdirectory(title='selecione'))
+    Label02['text'] = path
+    Label03['text'] = "Novo diretório selecionado!"
 
 
 def converter():
-    global pasta
+
+    global path, vjpge, barra_progresso, atualizar_progresso
+    fuction.del_filetext()
+    barra_progresso.config(value=0)
+    atualizar_progresso()
+
+    if vjpge.get() == 1:
+        time.sleep(2)
+        fuction.to_jpeg(path)
+        barra_progresso.config(value=30)
+        atualizar_progresso()
+
+    if vpng.get() == 1:
+        time.sleep(2)
+        fuction.to_png(path)
+        barra_progresso.config(value=60)
+        atualizar_progresso()
+
+    if vgif.get() == 1:
+        time.sleep(2)
+        fuction.to_gif(path)
+        barra_progresso.config(value=90)
+        atualizar_progresso()
+
+    barra_progresso.config(value=100)
+    atualizar_progresso()
 
 
 janela = ttk.Tk()
-janela.title('Conversor de Imagens')
 
 
+# Frame 01
 frame01 = ttk.Frame(janela)
-label01 = ttk.Label(
-    frame01, text='SELECIONE UM DIRETÓRIO', font=('helvetica', 15))
-Label02 = ttk.Label(frame01, text='INFORMAÇÕES APARECERAM AQUI', width='30',
-                    relief='groove')
-Label03 = ttk.Label(frame01, text='STATUS')
+label01 = ttk.Label(frame01, text='SELECIONE O DIRETÓRIO DOS ARQUIVOS',
+                    font=("Arial", 20, "bold"), foreground="white")
+Label02 = ttk.Label(frame01, text=path,
+                    width='50', relief='groove')
+Label03 = ttk.Label(frame01, text='')
 botao01 = ttk.Button(frame01, text='SELECIONAR',
                      command=get_diretorio)
 
+frame01.grid(column=0, row=0, sticky='n', pady=5)
+label01.grid(column=0, row=1, sticky='w', pady=5)
+Label02.grid(column=0, row=2, pady=5, sticky='w')
+Label03.grid(column=0, row=3, sticky='w', pady=5)
+botao01.grid(column=3, row=2, pady=5)
+
+# Frame 02
 
 frame02 = ttk.Frame(janela)
-checkbutton = ttk.Checkbutton(janela,)
+barra_rolagem = ttk.Scrollbar(frame02)
+texto_log1 = ttk.Text(frame02, borderwidth='1', width=80,
+                      height=10, relief="groove", yscrollcommand=barra_rolagem.set)
+barra_rolagem.config(command=texto_log1.yview)
+
+
+barra_progresso = Progressbar(
+    frame02, mode='determinate', length=550, value=0)
+
+Label04 = ttk.Label(
+    frame02, text='Selecione para que formato você deseja converter')
+
+
+def atualizar_progresso():
+    with open("conversion_log.txt", "r") as f:
+        texto_log1.delete(1.0, ttk.END)
+        texto_log1.insert(ttk.END, f.read())
+        janela.update()
+
+
+frame02.grid(column=0, row=2, pady=5)
+texto_log1.grid(column=0, row=0, sticky='w')
+barra_rolagem.grid(column=1, row=0, sticky='ns')
+barra_progresso.grid(column=0, row=2)
+Label04.grid(column=0, row=3, sticky='w', padx=5, pady=5)
+
+# Frame 03
+frame03 = ttk.Frame(janela)
+
+
+vjpge = ttk.IntVar()
+checkbutton_jpge = ttk.Checkbutton(
+    frame03, text='Jpge', variable=vjpge, onvalue=True, offvalue=False)
+
+vpng = ttk.IntVar()
+checkbutton_png = ttk.Checkbutton(
+    frame03, text='Png', variable=vpng, onvalue=True, offvalue=False)
+
+vgif = ttk.IntVar()
+checkbutton_gif = ttk.Checkbutton(
+    frame03, text='Gif', variable=vgif, onvalue=True, offvalue=False)
+
+
 botao02 = ttk.Button(
-    frame02, text='CONVERTER IMAGENS', command=converter)
+    frame03, text='CONVERTER IMAGENS', command=converter)
 
 
-frame03 = ttk.Frame(janela, borderwidth='1', width='25',
-                    height='10', relief="groove")
-Label04 = ttk.Label(frame03, text='A', borderwidth='1', width='25',
-                    height='10', relief="groove")
+frame03.grid(column=0, row=3, sticky='w')
+frame03.grid_columnconfigure(0, weight=1)
+frame03.grid_columnconfigure(1, weight=1)
+frame03.grid_columnconfigure(2, weight=1)
 
-
-botao01.grid(column=3, row=2, padx='5', sticky='w')
-botao02.grid(column=0, row=0)
-
-
-frame01.grid(column=0, row=0)
-frame02.grid(column=0, row=1, pady='5')
-frame03.grid(column=2, row=0, pady='5', sticky='s')
-
-
-label01.grid(column=0, row=0, sticky='w')
-Label02.grid(column=0, row=2)
-Label03.grid(column=0, row=3, sticky='w')
-Label04.grid(column=0, row=0, sticky='s')
+checkbutton_jpge.grid(column=0, row=1, padx=5, pady=5,
+                      sticky='w')
+checkbutton_png.grid(column=1, row=1, padx=5, pady=5,
+                     sticky='ew')
+checkbutton_gif.grid(column=2, row=1, padx=5, pady=5,
+                     sticky='e')
+botao02.grid(column=3, row=2, sticky='ew')
 
 
 janela.mainloop()
+fuction.del_filetext()
